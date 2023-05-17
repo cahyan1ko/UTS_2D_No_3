66673a238b1bf1a71ac64510bca6b485ef99edb8
cahyan1ko <116068280+cahyan1ko@users.noreply.github.com>
2023-05-17 07:16:05 +0700

    Add files via upload

---
+++ assets/requires/config.php
@@ -0,0 +1,6 @@
+<?php
+define('APPNAME', 'CAHYA NIKO NANDITA');
+define('PATH', 'http://localhost/');
+define('SITE_ROOT', __DIR__);
+define('MYROOT', $_SERVER['DOCUMENT_ROOT']);
+?>
---
+++ assets/requires/header1.php
@@ -0,0 +1,13 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo APPNAME; ?></title>
+<!-- BOOTSTRAP -->
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./sweetalert/sweetalert.css">
+<!-- JQUERY AND BOOTSTRAP SCRIP LOADING -->
+<script src="./jQuery/jquery-3.5.1.js"></script>
+<script src="./bootstrap/js/bootstrap.js"></script>
+<script src="./bootstrap/bootstrap.min.js"></script>
+<script src="./sweetalert/sweetalert.min.js"></script>
